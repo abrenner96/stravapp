8896dc9602b6053f26fbb21bafa5d812e4897e08
abrenner96 <94646495+abrenner96@users.noreply.github.com>
2022-04-18 13:47:25 -0400

    Add files via upload

---
+++ secrets_1.py
@@ -0,0 +1,13 @@
+'''
+
+stravapp
+
+secrets_1
+
+'''
+client_id = 81708
+API_KEY = 'd83dcd170c8adb84bd0c10b8978df4b1dc1559c1'
+access_token = "5a021198b561bd5338b01ecd2abf9e3233524478" 
+refresh_token = "f140a2d01e054e82427e275eaea6ca583ab69b6a"
+
+# some_code = adfdae241942e1ac13b5e80f99735c428e36c695
---
+++ stravapp.py
@@ -0,0 +1,192 @@
+from asyncio import SendfileNotAvailableError
+from urllib import response
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from secrets_1 import API_KEY
+from secrets_1 import client_id
+from secrets_1 import refresh_token
+# from secrets_1 import access_token
+import pandas as pd
+from pandas.io.json import json_normalize
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import json
+
+
+
+# API 
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': {client_id},
+    'client_secret': {API_KEY},
+    'refresh_token': {refresh_token},
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+#CACHING
+
+CACHE_FILE_NAME = 'strava_cache.json'
+
+
+
+def load_cache():
+  try: 
+    cache_file = open(CACHE_FILE_NAME, 'r')
+    cache_contents = cache_file.read()
+    cache = json.loads(cache_contents)
+    cache_file.close()
+  except:
+    cache = {}
+  return cache
+
+def store_in_cache_file(cache):
+  cache_contents = json.dumps(cache)
+  cache_file = open(CACHE_FILE_NAME, 'w')
+  cache_file.write(cache_contents)
+  cache_file.close()  
+
+def construct_unique_key(baseurl, params):
+    param_strings = []
+    connector = '_'
+    for k in params.keys():
+        param_strings.append(f'{k}_{params[k]}')
+    param_strings.sort()
+    unique_key = baseurl + connector +  connector.join(param_strings)
+    return unique_key
+
+strava_cache = load_cache()
+
+def make_request_with_cache(url):
+  request_key = construct_unique_key(url)
+  if request_key in strava_cache.keys():
+    print("Data from cache being used")
+    return strava_cache[request_key]
+  else:
+    print("Searching webpage for data")
+    response = requests.get(url)
+    strava_cache[request_key] = response.text
+    store_in_cache_file(strava_cache)
+    return strava_cache[request_key]
+
+
+# how to signal if more data needs to be cached  
+if load_cache() != strava_cache:
+  print("Requesting Token...\n")
+  res = requests.post(auth_url, data=payload, verify=False)
+  access_token = res.json()['access_token']
+  print("Access Token = {}\n".format(access_token))
+
+  header = {'Authorization': 'Bearer ' + access_token}
+  param = {'per_page': 200, 'page': 1}
+  df = requests.get(activites_url, headers=header, params=param).json()
+  store_in_cache_file(df)
+else:
+  activities = pd.json_normalize(strava_cache)
+
+
+# print(my_dataset[5]["name"])
+# print(activities.columns)
+# print(activities)
+
+#CREATING DATASET
+
+cols = ['name', 'upload_id', 'type', 'distance', 'moving_time',   
+         'average_speed', 'max_speed','total_elevation_gain',
+         'start_date_local', 'achievement_count'
+       ]
+activities = activities[cols]
+activities['start_date_local'] = pd.to_datetime(activities['start_date_local'])
+activities['start_time'] = activities['start_date_local'].dt.time
+activities['start_date_local'] = activities['start_date_local'].dt.date
+activities = activities.astype({'moving_time':'int'})
+# print(activities.dtypes)
+
+
+#CREATING SUBDATASETS
+runs = activities.loc[activities['type'] == 'Run']
+bikes = activities.loc[activities['type'] == 'Ride']
+
+# print(runs)
+# print(bikes)
+
+#CREATING PLOTS 
+
+plt.plot(runs['start_date_local'], (runs['distance']/1609.344), color='red', marker='o')
+plt.title('Distance Vs Date', fontsize=14)
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('Distance in Miles', fontsize=14)
+plt.grid(True)
+# plt.show()
+
+plt.plot(runs['start_date_local'], (runs['average_speed']/60), color='red', marker='o')
+plt.title('Average Speed Vs Date', fontsize=14)
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('Distance in Miles', fontsize=14)
+plt.grid(True)
+plt.show()
+
+fig = plt.figure(figsize=(10,7))
+ax = fig.add_axes([0,0,1,1])
+ax.bar(activities['type'],activities['total_elevation_gain'])
+fig.xlabel('Type of Activity')
+fig.ylabel('Elevation Gain')
+fig.title("Elevation gain by activity type")
+plt.show()
+
+plt.plot((runs['distance']/1609.344), runs['max_speed'],  color='red', marker='o')
+plt.title('Elevation gain Vs Distance', fontsize=14)
+plt.xlabel('Max Speed', fontsize=14)
+plt.ylabel('Distance in Miles', fontsize=14)
+plt.grid(True)
+# plt.show()
+
+#CREATING GRAPH/TREE
+class BST:
+  def __init__(self, key):
+    self.key = key
+    self.lchild = None
+    self.rchild = None
+  def insert(self, data):
+    if self.key is None:
+      self.key = data
+      return
+    if self.key > data:
+      if self.lchild:
+        self.lchild.insert(data)
+      else:
+        self.lchild = BST(data)
+    else:
+      if self.rchild:
+        self.rchild.insert(data)
+      else:
+        self.rchild = BST(data)
+
+
+moving_time_lst_r = (runs['moving_time']/60)
+moving_time_lst_r = moving_time_lst_r.astype(int)
+# print(f'moving type {moving_time_lst.dtypes}')
+# print(moving_time_lst_r)
+# root_r = BST(int(input("Running \n Enter a time in minutes: ")))
+
+# for i in moving_time_lst_r:
+#   root_r.insert(i)
+
+
+moving_time_lst_b = (bikes['moving_time']/60)
+moving_time_lst_b = moving_time_lst_b.astype(int)
+# print(f'moving type {moving_time_lst.dtypes}')
+# print(moving_time_lst_b)
+# root_b = BST(int(input("Biking \n Enter a time in minutes: ")))
+
+# for i in moving_time_lst_b:
+#   root_b.insert(i)
